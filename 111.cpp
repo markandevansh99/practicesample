cd28621733407531d4e165fa19f6c9af8457b33b
anuragmor <500069361@stu.upes.ac.in>
2019-04-18 21:51:12 +0530

    structure and class is created

--- 111.cpp
+++ 111.cpp
@@ -2,3 +2,28 @@
 #include<cstdio>
 #include<cstdlib>
 using namespace std;
+struct node
+{
+    int info;
+    struct node *next;
+}*start;
+
+class single_llist
+{
+    public:
+        node* create_node(int);
+        void insert_begin();
+        void insert_pos();
+        void insert_last();
+        void delete_pos();
+        void sort();
+        void search();
+        void update();
+        void reverse();
+        void display();
+        single_llist()
+        {
+            start = NULL;
+        }
+};
+
